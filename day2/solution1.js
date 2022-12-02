d25902c50f67442202794884c43bba6b4b019822
Jakub Romanczyk <j.romanczyk@gmail.com>
2022-12-02 10:41:39 +0100

    day2 solutions

---
+++ day2/solution1.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import readline from 'readline';
+
+const lineReader = readline.createInterface({
+    input: fs.createReadStream('inputs.txt')
+});
+
+let score = 0;
+
+const scoring = {
+    "AX": 4,
+    "AY": 8,
+    "AZ": 3,
+    "BX": 1,
+    "BY": 5,
+    "BZ": 9,
+    "CX": 7,
+    "CY": 2,
+    "CZ": 6,
+}
+
+lineReader.on('line', (line) => {
+    const pair = line.split(" ").join("");
+    score += scoring[pair];
+});
+
+lineReader.on('close', () => {
+    console.log(score);
+});
---
+++ day2/solution2.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import readline from 'readline';
+
+const lineReader = readline.createInterface({
+    input: fs.createReadStream('inputs.txt')
+});
+
+let score = 0;
+
+const scoring = {
+    "AX": 3,
+    "AY": 4,
+    "AZ": 8,
+    "BX": 1,
+    "BY": 5,
+    "BZ": 9,
+    "CX": 2,
+    "CY": 6,
+    "CZ": 7
+}
+
+lineReader.on('line', (line) => {
+    const pair = line.split(" ").join("");
+    score += scoring[pair];
+});
+
+lineReader.on('close', () => {
+    console.log(score);
+});
