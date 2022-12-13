889033d147def940eb941d639b4b2272ff10ab37
Jakub Romanczyk <j.romanczyk@gmail.com>
2022-12-13 23:22:22 +0100

    add jest and parseFile

--- utils/lineReader.ts
+++ utils/lineReader.ts
@@ -2,8 +2,28 @@ import fs from 'fs';
 import path from 'path';
 import readline from 'readline';
 
-export const initLineReader = (dir: string) => {
+export const initLineReader = (
+    dir: string,
+    filename: string = 'inputs.txt'
+) => {
     return readline.createInterface({
-        input: fs.createReadStream(path.join(dir, 'inputs.txt')),
+        input: fs.createReadStream(path.join(dir, filename)),
+    });
+};
+
+export const parseFile = async (
+    dir: string,
+    filename: string = 'inputs.txt'
+): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        try {
+            const lineReader = initLineReader(dir, filename);
+            const lines: string[] = [];
+
+            lineReader.on('line', (line) => lines.push(line));
+            lineReader.on('close', () => resolve(lines.join('\n')));
+        } catch (error) {
+            reject(`Error when reading file ${path.join(dir, filename)}`);
+        }
     });
 };
